d433749b5d3bc1cefbb50cc16e1ccf89d664af89
yoko <yoko@transposit.com>
2019-08-01 23:58:30 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -11,20 +11,20 @@
   const userId = parsed_body.user_id;
   const response_url = parsed_body.response_url;
 	console.log(parsed_body);
-  setImmediate(() => {
-    let user = api.user({type: "slack", workspaceId, userId});
-    if (user) {
-      let message = api.run('this.get_slack_message', {}, {asUser: user.id})[0];
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: message
-      });      
-    } else {
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: {text: 'Please configure your user'}
-      });      
-    }
-  });
+  // setImmediate(() => {
+  //   let user = api.user({type: "slack", workspaceId, userId});
+  //   if (user) {
+  //     let message = api.run('this.get_slack_message', {}, {asUser: user.id})[0];
+  //     api.run("slack_webhook.post_to_response_url", {
+  //       response_url: response_url,
+  //       post_body: message
+  //     });      
+  //   } else {
+  //     api.run("slack_webhook.post_to_response_url", {
+  //       response_url: response_url,
+  //       post_body: {text: 'Please configure your user'}
+  //     });      
+  //   }
+  // });
   return { status_code: 200 };
 }
