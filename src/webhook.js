34907a2838164798a20618a26ebcafa3c18922ca
yoko <yoko@transposit.com>
2019-08-02 22:23:20 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -11,9 +11,7 @@
   let text = parsed_body.attachements[0].text;
   
   if (text.startsWith("SUCCESS")) {
-
     let speedtext = text + "\n " + "API speed test results: \n" + api.run('this.run_monitored_endpoints');
-       
      api.run('this.post_to_slack', {msg: speedtext});
   } else {
     api.run('this.post_to_slack', {msg: text});
