b72bd61f323cd7640cbad799fd991269c0567855
yoko <yoko@transposit.com>
2019-08-02 22:42:34 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,10 +1,3 @@
-/**
- * This operation is an example of a JavaScript operation deployed as a Webhook
- * and configured to work with Slack.
- *
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/building/webhooks
- */
 ({ http_event }) => {
   const parsed_body = http_event.parsed_body;
   console.log(parsed_body);
