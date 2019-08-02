fe86f2812c5a1a29169c4dea1c58f9bdd30f4d39
yoko <yoko@transposit.com>
2019-08-02 00:30:02 +0000

    Commit from Transposit IDE

--- src/run_monitored_endpoints.js
+++ src/run_monitored_endpoints.js
@@ -1,6 +1,16 @@
 (params) => {
+  const _ = require("underscore.js");
   const monitorId = '1e9b4b35-7bb0-4210-ab01-d2aa8c684082';
-  return api.run('postman.run_monitor', {id: monitorId});
+  let result = api.run('postman.run_monitor', {id: monitorId})[0];
+  let assertions = result.run.stats.assertions;
+  let failedText = assertions.failed + " assertions failed.";
+  let executions = result.run.executions;
+  let speedText = "";
+  
+  _.each(executions, (e) => {
+    speedText = speedText + `${e.item.name} took *${e.response.responseTime}ms* \n`;
+  });
+  return speedText
 }
 
 /*
