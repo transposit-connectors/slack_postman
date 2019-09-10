dce07baaafc49a630632cad61c726683b3e368d8
Dan Moore <mooreds@transposit.com>
2019-09-10 23:01:03 +0000

    have to take first result

--- src/post_to_slack.js
+++ src/post_to_slack.js
@@ -1,5 +1,5 @@
 (params) => {
-    const body = api.run("this.response_body",{msg: params.msg});
+    const body = api.run("this.response_body",{msg: params.msg})[0];
     let result = api.run("slack.post_chat_message", {
         $body: JSON.stringify(body)
     });
