2026a7df6a57f5e26fc7e341b1f9fbe028477203
Dan Moore <mooreds@transposit.com>
2019-09-10 20:42:09 +0000

    post to slack

--- src/post_to_slack.js
+++ src/post_to_slack.js
@@ -1,38 +1,28 @@
 (params) => {
-  // post to slack
-  let blockToPost = 
-[
-	{
-		"type": "section",
-		"text": {
-			"type": "mrkdwn",
-			"text": params.msg
-		},
-		"accessory": {
-			"type": "button",
-			"text": {
-				"type": "plain_text",
-				"text": "Re-run Postman"
-			},
-			"value": "rerun",
-			"action_id": "rerunbutton"
-		}
-	}
-];
-  
-  const body = {
-    channel: "general",
-    text: "Hello! Your API performance test results are ready.",
-    blocks: blockToPost,
-    as_user: true
-  };
-  let result = api.run("slack.post_chat_message", {
-    $body: JSON.stringify(body)
-  });
-  console.log(result);
-}
+    let blockToPost = [{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": params.msg
+        },
+        "accessory": {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": "Re-run Postman"
+            },
+            "value": "rerun",
+            "action_id": "rerunbutton"
+        }
+    }];
 
-/*
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/references/js-operations
- */
+    const body = {
+        channel: "general",
+        text: "Hello! Your API performance test results are ready.",
+        blocks: blockToPost,
+        as_user: true
+    };
+    let result = api.run("slack.post_chat_message", {
+        $body: JSON.stringify(body)
+    });
+}
