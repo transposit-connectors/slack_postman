9c3263e8682adfa576b44d3545fcec67a23e30d9
Dan Moore <mooreds@transposit.com>
2019-09-10 22:59:08 +0000

    extract out block of response

--- src/post_to_slack.js
+++ src/post_to_slack.js
@@ -1,26 +1,5 @@
 (params) => {
-    let blockToPost = [{
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": params.msg
-        },
-        "accessory": {
-            "type": "button",
-            "text": {
-                "type": "plain_text",
-                "text": "Re-run Postman"
-            },
-            "value": "rerun",
-            "action_id": "rerunbutton"
-        }
-    }];
-
-    const body = {
-        channel: env.get('channelName'),
-        blocks: blockToPost,
-        as_user: true
-    };
+    const body = api.run("this.response_body",{msg: params.msg});
     let result = api.run("slack.post_chat_message", {
         $body: JSON.stringify(body)
     });
---
+++ src/response_body.js
@@ -0,0 +1,25 @@
+(params) => {
+     let blockToPost = [{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": params.msg
+        },
+        "accessory": {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": "Re-run Postman"
+            },
+            "value": "rerun",
+            "action_id": "rerunbutton"
+        }
+    }];
+
+    const body = {
+        channel: env.get('channelName'),
+        blocks: blockToPost,
+        as_user: true
+    };
+  return body;
+}
