c9973b4b9095740e7b946b6c72b32954d6c4a773
Dan Moore <mooreds@transposit.com>
2019-09-10 20:30:45 +0000

    check for button id before doing things

--- src/webhook_slack_button.js
+++ src/webhook_slack_button.js
@@ -1,10 +1,12 @@
 ({ http_event }) => {
   const parsed_body = http_event.parsed_body;  
-  console.log(JSON.parse(parsed_body.payload));
-  let speedtext =
+  const parsed_slack_response = JSON.parse(parsed_body.payload);
+  if (parsed_slack_response.actions[0].action_id = "rerunbutton") { 
+    const speedtext =
       "API speed test results (re-running):\n" +
       api.run("this.run_monitored_endpoints");
       api.run("this.post_to_slack", { msg: speedtext }, {});
+  }
   
   return {
     status_code: 200,
