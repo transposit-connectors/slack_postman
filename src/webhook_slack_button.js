8486cb7a3c2cab7831d3492bd3dffdbf6f8a6a0d
Dan Moore <mooreds@transposit.com>
2019-09-11 16:10:06 +0000

    reply in thread

--- src/webhook_slack_button.js
+++ src/webhook_slack_button.js
@@ -11,6 +11,7 @@
             "API speed test results (re-running):\n" + 
             api.run("this.run_monitored_endpoints");
    const body = api.run("this.response_body",{msg: speedtext})[0];
+      body.thread_ts = parsed_slack_response.message.ts;
       api.run("slack_webhook.post_to_response_url", {
       post_body: body,
       response_url: response_url
