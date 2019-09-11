54a86773d2cf07e86af80e26c72c15485863d4a0
Dan Moore <mooreds@transposit.com>
2019-09-11 16:26:42 +0000

    corrected typo, beautified

--- src/response_body.js
+++ src/response_body.js
@@ -1,27 +1,28 @@
 (params) => {
-     let blockToPost = [{
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": params.msg
-        }];
-     if (params.button) {
-       blockToPost.push({
-        "accessory": {
-            "type": "button",
+    let blockToPost = [{
+            "type": "section",
             "text": {
-                "type": "plain_text",
-                "text": "Re-run Postman"
-            },
-            "value": "rerun",
-            "action_id": "rerunbutton"
+                "type": "mrkdwn",
+                "text": params.msg
+            }];
+        if (params.button) {
+            blockToPost.push({
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Re-run Postman"
+                    },
+                    "value": "rerun",
+                    "action_id": "rerunbutton"
+                }
+            });
         }
-    });
 
-    const body = {
-        channel: env.get('channelName'),
-        blocks: blockToPost,
-        as_user: true
-    };
-  return body;
-}
+        const body = {
+            channel: env.get('channelName'),
+            blocks: blockToPost,
+            as_user: true
+        };
+        return body;
+    }
